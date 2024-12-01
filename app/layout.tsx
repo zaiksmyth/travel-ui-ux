65dd6ee6860aae79b11417c34db59a2ccee87787
PS Parwez <infoparwez200@gmail.com>
2024-12-01 11:34:34 +0530

    Fix:favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* svg favicon  */}
+        {/*  favicon  */}
         <link rel="icon" href="/icon/favicon.svg" type="image/svg+xml" />
 
       </head>
